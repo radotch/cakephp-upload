4601c65b2d5d177b3f64ab1abff8ba1bc293edfe
Radoslav Cholakov <tcholakov_radoslav@yahoo.com>
2019-02-27 16:48:38 +0200

    Rebuilt configuration and set column type of configured fields.

--- src/Model/Behavior/UploadableBehavior.php
+++ src/Model/Behavior/UploadableBehavior.php
@@ -3,6 +3,8 @@ namespace CakeUpload\Model\Behavior;
 
 use Cake\ORM\Behavior;
 use Cake\ORM\Table;
+use Cake\Database\Type;
+use CakeUpload\Database\Type\UploadedFileType;
 
 /**
  * Uploadable behavior
@@ -16,4 +18,36 @@ class UploadableBehavior extends Behavior
      * @var array
      */
     protected $_defaultConfig = [];
+    
+    /**
+     * Rebuild configuration.
+     * Change column type for fields in table schema.
+     * 
+     * @param array $config
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        $configs = [];
+        foreach ($this->getConfig(NULL, []) as $field => $settings) {
+            if (is_int($field)) {
+                $configs[$settings] = [];
+                $this->_configDelete($field);
+            } else {
+                $configs[$field] = $settings;
+            }
+        }
+        
+        $this->setConfig($configs);
+        
+        // Set column type for configured fields (UploadedFileType)
+        Type::map('uploadedFile', UploadedFileType::class);
+        
+        $schema = $this->_table->getSchema();
+        foreach (array_keys($this->getConfig(NULL, [])) as $field) {
+            $schema->setColumnType($field, 'uploadedFile');
+        }
+        
+        $this->_table->setSchema($schema);
+    }
 }
